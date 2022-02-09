6198a530a745e7476c7841471fad8f3a2f1c68d5
ylf <iivodka5@gmail.com>
2022-02-09 22:28:02 +0000

    heritage

--- Pricer/main/CF_BS.cpp
+++ Pricer/main/CF_BS.cpp
@@ -7,7 +7,7 @@ CF_BS::CF_BS(double r, double T, double alpha, int N, double eta, double s0, dou
 
 // Characteristic Function for BS model
 complex<double> CF_BS::phi(const std::complex<double> t) const {
-    complex<double> i(0.0, 1.0);
+    complex <double> i(0.0, 1.0);
     complex<double> phi = exp(i * t * (s0 + (r - 0.5 * v * v) * T) - 0.5 * v * v * t * t * T);
     return phi;
 }
